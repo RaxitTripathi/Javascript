1a1a7771958071bb6f72c5043d74e428e8f343c3
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-07 12:30:44 +0530

    While Loop

---
+++ 05Iteration/whileLoop.js
@@ -0,0 +1,24 @@
+
+// while loop
+// Used to repeat a block of code as long as the condition is true.
+
+// Syntax:
+// while(condition){
+//     code
+// }
+
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
