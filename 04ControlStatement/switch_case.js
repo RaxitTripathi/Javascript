4a38cbe8c25395d8776713eff4c85374cd6a6ce2
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-07 12:25:38 +0530

    switch case

---
+++ 04ControlStatement/switch_case.js
@@ -0,0 +1,38 @@
+// switch case statement
+// Used when we have multiple choices for one value.
+// It compares the value with different cases.
+
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    case 3:
+        console.log("Wednesday");
+        break;
+
+    case 4:
+        console.log("Thursday");
+        break;
+
+    case 5:
+        console.log("Friday");
+        break;
+
+    case 6:
+        console.log("Saturday");
+        break;
+
+    case 7:
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("Invalid day");
+}
