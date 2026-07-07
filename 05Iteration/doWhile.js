49f36178856350c54611045e00a8267717b8bec6
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-07 12:30:57 +0530

    Do while Loop

---
+++ 05Iteration/doWhile.js
@@ -0,0 +1,25 @@
+
+// do while loop
+// Similar to while loop, but it executes the code at least once
+// even if the condition is false.
+
+// Syntax:
+// do {
+//     code
+// } while (condition);
+
+
+let i = 1;
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
+
+
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
