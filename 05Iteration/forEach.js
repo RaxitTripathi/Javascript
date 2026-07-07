5f03b746d0d15253985551c74df799f08f61c304
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-07 12:31:11 +0530

    For Each Loop

---
+++ 05Iteration/forEach.js
@@ -0,0 +1,24 @@
+
+// forEach loop
+// Used to execute a function once for each element of an array.
+// Mostly used with arrays.
+
+// Syntax:
+// array.forEach(function(element){
+//     code
+// });
+
+
+let numbers = [10, 20, 30, 40, 50];
+
+numbers.forEach(function(num) {
+    console.log(num);
+});
+
+
+// Output:
+// 10
+// 20
+// 30
+// 40
+// 50
