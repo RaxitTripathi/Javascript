a412b9f7f0a8e7f339ff9bf2185f2bb2a98949de
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-07 11:13:23 +0530

    What is NaN?

---
+++ 02DataTypes+TypeSystem/notAnNumber.js
@@ -0,0 +1,10 @@
+// NaN (Not a Number)
+// A special value representing an invalid numeric result.
+
+// Examples:
+console.log(0 / 0);           // NaN
+console.log(Number("Hello")); // NaN
+console.log("abc" * 2);       // NaN
+
+// Check NaN
+console.log(Number.isNaN(NaN)); // true
