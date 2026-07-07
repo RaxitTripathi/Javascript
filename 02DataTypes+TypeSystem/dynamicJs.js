cbab84d7fdf71b0ac6ade1bdfd759856fcfbaf76
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-07 11:12:01 +0530

    What is Dynamic Typing?And why js is DynamicTyping

---
+++ 02DataTypes+TypeSystem/dynamicJs.js
@@ -0,0 +1,18 @@
+// Dynamic Typing:
+// - Variable type is decided at runtime.
+// - No need to specify a data type (int, string, etc.).
+// - The same variable can store different data types.
+// - JavaScript automatically detects the type from the assigned value.
+// - That's why JavaScript is called a dynamically typed language.
+
+let data = 10;        // Number
+data = "Hello";       // String
+data = true;          // Boolean
+
+console.log(data);    // true
+
+console.log(typeof data)
+
+
+
+// BTW it is not compatable in programing world thats why we learn TYPESCRIPT
