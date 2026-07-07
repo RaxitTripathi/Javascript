4b7af7412e7dde2e94a2c530c65f331d7cf958c6
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-07 12:25:13 +0530

    If ,else if ,else

---
+++ 04ControlStatement/if_else.js
@@ -0,0 +1,20 @@
+// if else if else statement
+// Used to execute different blocks of code based on conditions
+
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("Grade A+");
+}
+else if (marks >= 75) {
+    console.log("Grade A");
+}
+else if (marks >= 60) {
+    console.log("Grade B");
+}
+else if (marks >= 40) {
+    console.log("Grade C");
+}
+else {
+    console.log("Fail");
+}
