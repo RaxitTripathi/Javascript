fc88bc29e5e34fc7709b1bf7e9820e55ec670ca3
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-08 18:41:45 +0530

    Object distructing

---
+++ 08Objects/object_distructing.js
@@ -0,0 +1,33 @@
+const student = {
+    name: "Rakshit",
+    age: 22,
+    course: "B.Tech"
+};
+
+// Object Destructuring
+const { name, age, course } = student;
+
+console.log(name);    // Rakshit
+console.log(age);     // 22
+console.log(course);  // B.Tech
+
+
+
+
+// or rename variable
+
+
+
+
+
+const students = {
+    name: "Rakshit",
+    age: 22
+};
+
+// Rename while destructuring
+const { name: studentName, age: studentAge, sclass = "Btech 3rd" } = students;
+
+console.log(studentName); // Rakshit
+console.log(studentAge);  // 22
+console.log(sclass);       // Here  sclass=btech 3rd is default value if sclass not present in students
