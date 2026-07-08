b967d4385ba87d0a170e4f936d8aa01ae64a1ecf
RaxitTripathi <raxit08tripathi@gmail.com>
2026-07-08 18:41:14 +0530

    Objects Basics

---
+++ 08Objects/objectBasic.js
@@ -0,0 +1,16 @@
+/* Objects in JavaScript are like real-world records – a collection of key-value pairs.
+They help us store structured data (like a student, a product, or a user profile).
+ */
+
+
+let student = {
+name: "Ravi",
+age: 21,
+isEnrolled: true
+};
+
+
+/* 
+🔑 Key-Value Structure
+Keys are always strings (even if you write them as numbers or identifiers)
+Values can be anything – string, number, array, object, function, etc. */
---
+++ 08Objects/objectBasic02.js
@@ -0,0 +1,16 @@
+let student = {
+name: "Ravi",
+age: 21,
+isEnrolled: true
+};
+
+
+console.log(student["name"]);
+// or
+console.log(student.name);
+
+
+/* Dot vs Bracket Notation
+
+Use dot notation for fixed key names
+Use bracket notation for dynamic or multi-word keys */
